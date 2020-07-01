ec2b32f8fbc2061356978ee18a9614efdb5bc181
明天啊 <173854134@qq.com>
2020-07-01 15:05:34 +0800

    底层基础路由

---
+++ src/routers/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import movieRouter from './movie'
+import mineRouter from './mine'
+import cinemaRouter from './cinema'
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes:[
+    movieRouter,
+    mineRouter,
+    cinemaRouter
+  ]
+})
+
+export default router
---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>hello Cinema</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
